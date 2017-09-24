42803375fdf9d0d9f960e191a36839772faf709b
Unknown <bosefalk@gmail.com>
2017-09-24 15:03:11 +0200

    Added specific leave conditions

--- cmd_wesnoth_FoB.sh
+++ cmd_wesnoth_FoB.sh
@@ -4,15 +4,20 @@ while read line;
         # expr evaluates STRING:REGEX, and instead of printing output sends it $
         # and executes the command after then
         # .* matches any character unlimited times, \(...\) matches the group .$
-        if expr "$line" : '.*\(bose\).*\(joined\).*\(nest\)' >/dev/null;  
+        if expr "$line" : '.*\(bose\).*\(joined\).*\(FoB\)' >/dev/null;  
 				then xdotool key --window 14680067 colon c o n t r o l space 1 space b o s e Return
  
-        elif expr "$line" : '.*\(felix\).*\(joined\).*\(nest\)' >/dev/null;  
+        elif expr "$line" : '.*\(felix\).*\(joined\).*\(FoB\)' >/dev/null;  
                 then xdotool key --window 14680067 colon c o n t r o l space 2 space f e l i x Return
                 
-		elif expr "$line" : '.*\(left\).*\(nest\)' >/dev/null;
+		elif expr "$line" : '.*\(bose\).*\(left\).*\(FoB\)' >/dev/null;
         	then sleep 2; xdotool key --window 14680067 Down Return;  
             sleep 2; xdotool key --window 14680067 Down Return 
+
+        elif expr "$line" : '.*\(felix\).*\(left\).*\(FoB\)' >/dev/null;
+            then sleep 2; xdotool key --window 14680067 Down Return;  
+            sleep 2; xdotool key --window 14680067 Down Return 
+
         fi
 done
 
