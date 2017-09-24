640db2a39c985f7525a7dcb31df5002af5b2fe93
Unknown <bosefalk@gmail.com>
2017-09-24 14:13:23 +0200

    more typos in FoB.sh

--- cmd_wesnoth_FoB.sh
+++ cmd_wesnoth_FoB.sh
@@ -5,10 +5,10 @@ while read line;
         # and executes the command after then
         # .* matches any character unlimited times, \(...\) matches the group .$
         if expr "$line" : '.*\(bose\).*\(joined\).*\(nest\)' >/dev/null;  
-				then xdotool key --window 14680067 colon c o n t r o l space 1 $
+				then xdotool key --window 14680067 colon c o n t r o l space 1 space b o s e Return
  
         elif expr "$line" : '.*\(felix\).*\(joined\).*\(nest\)' >/dev/null;  
-                then xdotool key --window 14680067 colon c o n t r o l space 2 $
+                then xdotool key --window 14680067 colon c o n t r o l space 2 space f e l i x Return
                 
 		elif expr "$line" : '.*\(left\).*\(nest\)' >/dev/null;
         	then sleep 2; xdotool key --window 14680067 Down Return;  
